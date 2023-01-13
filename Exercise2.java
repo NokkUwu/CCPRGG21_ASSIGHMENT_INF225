819cc1b825ed3bcdb8042ec3b34b2021914b9043
Nokkt <119907355+Nokkt@users.noreply.github.com>
2023-01-13 10:56:32 +0800

    Create Exercise2.java

---
+++ Exercise2.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class App {
+
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+
+        train("Driver");
+        scan.close();
+    }
+
+    static void train(String school) {
+
+        System.out.println("Driving!");
+        test(school);
+
+        String feedback = getFeedBack(school);
+
+        // Define the base case / stopping condition
+        if (feedback.equals("no")) {
+
+            // Do something to reach the goal
+            fixRecipe(school);
+
+            // Recursive call
+            train(school);
+        } else if (feedback.equals("yes")) {
+            info(school);
+        }
+    }
+
+    static void test(String school) {
+        System.out.println("Testing Driving Skills");
+    }
+
+    static String getFeedBack(String school) {
+        System.out.println("Is the " + school + " good enough? (Enter \"yes\" or \"no\")");
+
+        String feedback = scan.next();
+
+        return feedback;
+    }
+
+    static void fixRecipe(String school) {
+        System.out.print("Enter additional driving lesson:");
+        String lesson = scan.next();
+        System.out.println("Fixing driving. Learned " + ingredient);
+    }
+
+    static void info(String school) {
+        System.out.println(school + " is good to get license.");    }
+}
